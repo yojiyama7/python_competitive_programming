b5621ed75ae9baf8c1b2f76745aca23c4d89dcea
yojiyama7 <yojiyama7@gmail.com>
2020-08-29 13:32:17 +0900

    abc d

--- atcoder/abc/abc_149/d_.py
+++ atcoder/abc/abc_149/d_.py
@@ -1,24 +1,69 @@
+# K個の独立した問題として考える
+
 N, K = map(int, input().split())
 RSP = list(map(int, input().split()))
 T = input()
 
-t = [to_num(t_i) for t_i in T]
+def is_win(me, other):
+    if me == 0 and other == 1:
+        return True
+    elif me == 1 and other == 2:
+        return True
+    elif me == 2 and other == 0:
+        return True
+    return False
 
-def to_num(hand):
-    return "rsp".find(hand)
+def char_to_num(s):
+    if s == "r":
+        return 0
+    elif s == "s":
+        return 1
+    else:
+        return 2
 
-# 0:あいこ, 1:勝ち, 2:負け
-def check_result(m, e):
-    return (e-m)%3
+# dp[i][j] = i回目のじゃんけんでjを出して取れる最大の点数([i%K]ごとに独立)
+dp = [[-1 for j in range(3)] for i in range(N+1)]
+dp[0] = [0, 0, 0]
+for i in range(1, N+1):
+    t = char_to_num(T[i-1])
+    if i <= K:
+        for j in range(3):
+            dp[i][j] = is_win(j, t)*RSP[j]
+    else:    
+        for j in range(3):
+            dp[i][j] = max(
+                [dp[i-K][k] + is_win(j, t)*RSP[j] for k in range(3) if j != k]
+            )
 
-dp = [[0 for _ in range(3)] for _ in range(N+1)]
-for i in range(1, K+1):
-    for j in range(3):
-        p = check_result(j, t[i-1])
-        dp[i][j] = max(dp[i-1]) + RSP[j]*(p==1)
+# print(dp)
 
-for i in range(K+1, N+1):
-    for j in range(3):
-        p = check_result(j, t[i-1])
-        RSP[]*(p==1)
-        dp[i][j] = 
+r = sum(map(max, dp[-K:]))
+print(r)
+################################
+
+# NOT AC
+
+# N, K = map(int, input().split())
+# RSP = list(map(int, input().split()))
+# T = input()
+
+# t = [to_num(t_i) for t_i in T]
+
+# def to_num(hand):
+#     return "rsp".find(hand)
+
+# # 0:あいこ, 1:勝ち, 2:負け
+# def check_result(m, e):
+#     return (e-m)%3
+
+# dp = [[0 for _ in range(3)] for _ in range(N+1)]
+# for i in range(1, K+1):
+#     for j in range(3):
+#         p = check_result(j, t[i-1])
+#         dp[i][j] = max(dp[i-1]) + RSP[j]*(p==1)
+
+# for i in range(K+1, N+1):
+#     for j in range(3):
+#         p = check_result(j, t[i-1])
+#         RSP[]*(p==1)
+#         dp[i][j] = 
--- atcoder/abc/abc_167/d_teleporter.py
+++ atcoder/abc/abc_167/d_teleporter.py
@@ -1,29 +1,66 @@
-# 無限ループには入る前aこ
-# 無限ループ内のb個
+# すぐにループしない形の時、ループに入らず終わるパターンを考慮する
 
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 
-a = [Ai-1 for Ai in A]
+A = [-1] + A
+
+loop_pos = None
+loop_start_time = None
+loop_end_time = None
+visited = [-1]*(N+1)
+pos = 1
+i = 0
+while visited[pos] == -1:
+    visited[pos] = i
+    pos = A[pos]
+    i += 1  
+    if visited[pos] != -1:
+        loop_pos = pos
+        loop_start_time = visited[pos]
+        loop_end_time = i
+
+# print(loop_start_time, loop_end_time)
+w = loop_end_time - loop_start_time
+K = min(K, loop_start_time) + max(0, K-loop_start_time)%w
+# print(f"K: {K}")
+
+pos = 1
+for i in range(K):
+    # print(pos)
+    pos = A[pos]
+print(pos)
+
+################################
+
+# # NOT AC
+
+# # 無限ループには入る前aこ
+# # 無限ループ内のb個
+
+# N, K = map(int, input().split())
+# A = list(map(int, input().split()))
+
+# a = [Ai-1 for Ai in A]
 
 
-visited = [0]*N
-visited[0] = 1
-p = 0
-l = [0]
-loop_start = None
-while True:
-    p = a[p]
-    if visited[p]:
-        loop_start = l.index(p)
-        break
-    visited[p] = 1
-    # print(p, visited)
-    l.append(p)
+# visited = [0]*N
+# visited[0] = 1
+# p = 0
+# l = [0]
+# loop_start = None
+# while True:
+#     p = a[p]
+#     if visited[p]:
+#         loop_start = l.index(p)
+#         break
+#     visited[p] = 1
+#     # print(p, visited)
+#     l.append(p)
 
-# print(l, loop_start, K)
+# # print(l, loop_start, K)
 
-if K < loop_start:
-    print(l[K])
-else:
-    print(l[loop_start + (K-loop_start)%(len(l)-loop_start)]+1)
+# if K < loop_start:
+#     print(l[K])
+# else:
+#         print(l[loop_start + (K-loop_start)%(len(l)-loop_start)]+1)
