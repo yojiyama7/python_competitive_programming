b4cce58070b37a8bd614cf3abb856279b381d521
yojiyama7 <yojiyama7@gmail.com>
2020-09-07 10:45:37 +0900

    adjust

--- atcoder/abc/abc_155/d_pairs.py
+++ atcoder/abc/abc_155/d_pairs.py
@@ -1,23 +1,30 @@
 # 正負の数の積に関するものは、積が負, 0, 正になるパターンで場合分けする
-
 # K番目(二分探索感)
 
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 A.sort()
 
-K
 
-# l = []
-# m = []
-# for i, a in enumerate(A):
-#     for j, b in enumerate(A):
-#         if i < j:
-#             l.append(a*b)
-#             m.append((a, b))
 
-# l.sort()
+################################
 
-# print(l[K-1])
+# N, K = map(int, input().split())
+# A = list(map(int, input().split()))
+# A.sort()
 
-# [print(x) for x in sorted(m, key=lambda x: x[0]*x[1])]
+# K
+
+# # l = []
+# # m = []
+# # for i, a in enumerate(A):
+# #     for j, b in enumerate(A):
+# #         if i < j:
+# #             l.append(a*b)
+# #             m.append((a, b))
+
+# # l.sort()
+
+# # print(l[K-1])
+
+# # [print(x) for x in sorted(m, key=lambda x: x[0]*x[1])]
