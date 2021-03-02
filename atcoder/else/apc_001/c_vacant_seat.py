88f708aa495d72c560a3a0e075f39c91fde0ec1c
YAYOSPC\yayo256 <yojiyama7@gmail.com>
2021-03-02 09:33:23 +0900

    BcfB hard END!!!

--- atcoder/else/apc_001/c_vacant_seat.py
+++ atcoder/else/apc_001/c_vacant_seat.py
@@ -1,14 +1,23 @@
+# インタラクティブおもろい
+
 N = int(input())
 
-s, e = 0, N
-print(s)
-s_num = int(input())
+print(0)
+
+s_num = ["Male", "Female", "Vacant"].index(input())
+if s_num == 2:
+    exit()
+    
+ok, ng = 0, N
 while True:
-    mid = (s + e) // 2
+    mid = (ok + ng) // 2
     print(mid, flush=True)
-    a = int(input())
-    is_right = s_num + (mid - s)
+    a = ["Male", "Female", "Vacant"].index(input())
+    if a == 2:
+        exit()
+    is_right = (a == (s_num + (mid - ok)) % 2)
     if is_right:
-        e = mid
+        ok = mid
+        s_num = a
     else:
-        s = mid
+        ng = mid
