ffccf75e7ae867e1ea7311b99324655ebe84adef
YAYOSPC\yayo256 <yojiyama7@gmail.com>
2021-03-07 13:38:59 +0900

    abc 194

---
+++ structs/heap.py
@@ -0,0 +1,10 @@
+class HeapMin:
+    def __init__(self, l):
+        self.l = l
+        heapify(self.l)
+
+    def pop(self):
+        return heappop(self.l)
+
+    def push(self, v):
+        heappush(self.l, v)
