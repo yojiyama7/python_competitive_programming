28bc400c94c5f7bb30412616b57fca17f372ef35
yojiyama7 <yojiyama7@gmail.com>
2021-06-21 09:04:36 +0900

    abc 206 e むずすぎ

---
+++ atcoder/abc/abc_206/e_divide_both.py
@@ -0,0 +1,24 @@
+# わからな強すぎ
+# よく俺このコード書けたな
+
+INF = 10**7
+
+L, R = map(int, input().split())
+
+l = [None]*(R+1)
+ans = 0
+# gcd(x, y) == i となるものを数える
+for i in reversed(range(2, R+1)):
+    l[i] = (R//i - (L-1)//i)**2
+    for j in range(2, INF):
+        if i*j > R:
+            break
+        l[i] -= l[i*j]
+    ans += l[i]
+
+# x == g or y == g を引く
+for i in range(2, R+1):
+    if L <= i <= R:
+        ans -= 2*(R//i - (L-1)//i) - 1
+
+print(ans)
