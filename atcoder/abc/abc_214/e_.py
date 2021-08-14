59efe1fed421f1fad4cf3724e04605e96cd783c4
yojiyama7 <yojiyama7@gmail.com>
2021-08-15 03:36:49 +0900

    edit

--- atcoder/abc/abc_214/e_.py
+++ atcoder/abc/abc_214/e_.py
@@ -1,26 +1,29 @@
-# 区間が与えられたとき l の時点で r を priority queue に追加するという典型
 
-T = int(input())
+################################
 
-ans_list = []
-for _ in range(T):
-    N = int(input())
-    LR = [list(map(int, input().split())) for _ in range(N)]
+# # 区間が与えられたとき l の時点で r を priority queue に追加するという典型
 
-    LR.sort()
-    bl, br = -1, -1
-    x = -1
-    ans = "Yes"
-    for l, r in LR:
-        if br < l:
-            x = l
-        else:
-            x = max(x+1, l)
-        # print((l, r), x)
-        if r < x:
-            ans = "No"
-            break
-        bl, br = l, r
-    ans_list.append(ans)
+# T = int(input())
 
-print(*ans_list, sep='\n')
+# ans_list = []
+# for _ in range(T):
+#     N = int(input())
+#     LR = [list(map(int, input().split())) for _ in range(N)]
+
+#     LR.sort()
+#     bl, br = -1, -1
+#     x = -1
+#     ans = "Yes"
+#     for l, r in LR:
+#         if br < l:
+#             x = l
+#         else:
+#             x = max(x+1, l)
+#         # print((l, r), x)
+#         if r < x:
+#             ans = "No"
+#             break
+#         bl, br = l, r
+#     ans_list.append(ans)
+
+# print(*ans_list, sep='\n')
