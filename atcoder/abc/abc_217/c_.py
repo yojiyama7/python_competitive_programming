5d0e9f64bafc3ba642f2813e3ccf70736e8a0ba7
yojiyama7 <yojiyama7@gmail.com>
2021-09-04 23:02:29 +0900

    abc 217 b

--- atcoder/abc/abc_217/c_.py
+++ atcoder/abc/abc_217/c_.py
@@ -1,8 +1,18 @@
 N = int(input())
 P = list(map(int, input().split()))
 
-ans = [-1]*N
-for i, p in enumerate(P):
-    ans[p-1] = i+1
-
+q = [(p, i) for i, p in enumerate(P, start=1)]
+q.sort()
+ans = [y for _, y in q]
 print(*ans)
+
+################################
+
+# N = int(input())
+# P = list(map(int, input().split()))
+
+# ans = [-1]*N
+# for i, p in enumerate(P):
+#     ans[p-1] = i+1
+
+# print(*ans)
