3b0582168831602e83c3c33bb73a67adf00b3a4e
yojiyama7 <yojiyama7@gmail.com>
2021-09-11 22:49:16 +0900

    b

--- atcoder/abc/abc_218/b_.py
+++ atcoder/abc/abc_218/b_.py
@@ -1,5 +1,18 @@
 P = list(map(int, input().split()))
 
-l = [p-1 for p in P]
-ans = ''.join(chr(ord('a') + li) for li in l)
+table = ""
+for i in range(26):
+    table += chr(ord('a') + i)
+ans = ""
+for p in P:
+    ans += table[p-1]
+
 print(ans)
+
+################################
+
+# P = list(map(int, input().split()))
+
+# l = [p-1 for p in P]
+# ans = ''.join(chr(ord('a') + li) for li in l)
+# print(ans)
