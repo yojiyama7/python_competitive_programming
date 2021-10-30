02e40599e946c89893cc292bc4866b81c31c3255
yayo256 <yojiyama7@gmail.com>
2021-10-30 23:04:33 +0900

    abc 225

--- atcoder/abc/abc_225/b_.py
+++ atcoder/abc/abc_225/b_.py
@@ -1,8 +1,24 @@
+from itertools import chain
+from collections import Counter
+
 N = int(input())
 AB = [list(map(int, input().split())) for _ in range(N-1)]
 
-a0, b0 = AB[0]
-if all(a0 in ab for ab in AB) or all(b0 in ab for ab in AB):
+c = Counter(chain(*AB))
+many_egdes_cnt = sum(v > 1 for v in c.values())
+if many_egdes_cnt == 1:
     print("Yes")
 else:
     print("No")
+
+
+################################
+
+# N = int(input())
+# AB = [list(map(int, input().split())) for _ in range(N-1)]
+
+# a0, b0 = AB[0]
+# if all(a0 in ab for ab in AB) or all(b0 in ab for ab in AB):
+#     print("Yes")
+# else:
+#     print("No")
