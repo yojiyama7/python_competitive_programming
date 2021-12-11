ea22a3668e97084e0f363fb9a6c28d6eaf9ad4d8
yayo256 <yojiyama7@gmail.com>
2021-12-11 19:51:04 +0900

    some easy and abc 231 init

--- atcoder/abc/abc_106/d_atcoder_express_2.py
+++ atcoder/abc/abc_106/d_atcoder_express_2.py
@@ -1,7 +1,56 @@
-import numpy as np
+# 1-indexed 閉区間 add_point sum_range
+class Bit:
+    def __init__(self, n):
+        self.n = n
+        self.array = [0]*(n+1)
 
-n, m, q = map(int, input().split(" "))
-lr = [tuple(map(int, input().split(" "))) for _ in range(m)]
-pq = [tuple(map(int, input().split(" "))) for _ in range(q)]
+    def add(self, x, w=1):
+        # print(f"{id(self)}: Bit.add({x}, {w})")
+        while (x <= self.n):
+            self.array[x] += w
+            x += (x & -x)
 
+    def sum(self, x, y=None):
+        # print(f"{id(self)}: Bit.sum({x}, {y})")
+        if y == None:
+            # 1~x
+            sum_num = 0
+            while (x > 0):
+                sum_num += self.array[x]
+                x -= (x & -x)
+            return sum_num
+        else:
+            # x~y
+            return self.sum(y) - self.sum(x-1)
 
+from itertools import accumulate as acc
+
+N, M, Q = map(int, input().split())
+LR = [tuple(map(int, input().split())) for _ in range(M)]
+PQ = [tuple(map(int, input().split())) for _ in range(Q)]
+
+Q_P = dict((i, set()) for i in range(1, N+1))
+for p, q in PQ:
+    Q_P[q].add(p)
+R_L = dict((i, []) for i in range(1, N+1))
+for l, r in LR:
+    R_L[r].append(l)
+part = Bit(N)
+ans_dict = dict()
+cnt = 0
+# print(R_L, Q_P)
+for r in range(1, N+1):
+    cnt += len(R_L[r])
+    for l in R_L[r]:
+        part.add(l)
+    # print([part.sum(i) for i in range(1, N+1)])
+    for l in Q_P[r]:
+        # print(l, part.sum(l-1))
+        ans = cnt - part.sum(l-1)
+        ans_dict[(l, r)] = ans
+    # print(r, "cnt: ", cnt)
+
+# print(ans_dict)
+
+for p, q in PQ:
+    print(ans_dict[(p, q)])
--- atcoder/abc/abc_230/e_fraction_floor_sum.py
+++ atcoder/abc/abc_230/e_fraction_floor_sum.py
@@ -6,27 +6,36 @@ def easy(x):
         ans += x//i
     return ans
 
-def root_floor(x):
-    ok, ng = 0, x
-    while abs(ok-ng) > 1:
-        mid = (ok+ng)//2
-        if mid**2 <= x:
-            ok = mid
-        else:
-            ng = mid
-    return ok
+def solve(N):
+    if N == 1:
+        return 1
+    def root_floor(x):
+        ok, ng = 0, x
+        while abs(ok-ng) > 1:
+            mid = (ok+ng)//2
+            if mid**2 <= x:
+                ok = mid
+            else:
+                ng = mid
+        return ok
 
-def div_ceil(a, b):
-    return -(-a//b)
+    N_root_floor = root_floor(N)
+    # print(f"root: {N_root_floor}")
 
-ans = 0 
-for i in range(1, root_floor(N)+1):
-    l_mt = N//(i+1)
-    r_le = div_ceil(N, i)
-    cnt = r_le-l_mt - (l_mt < i <= r_le)
-    score = i*cnt
-    print(i, N//i, (l_mt+1, r_le+1), (l_mt < i <= r_le), score)
-    ans += N//i + score
+    ans = 0 
+    for i in range(1, N_root_floor+1):
+        ans += N//i
+    # print(ans)
+    for i in range(1, N_root_floor+1):
+        l_open = N//(i+1)
+        r_close = N//i
+        cnt = r_close-l_open - (l_open < i <= r_close)
+        # print(i, (l_open, r_close), cnt)
+        ans += i*cnt
+    return ans
 
-print(ans)
-print(easy(N))
+print(solve(N))
+# for i in range(1, 10000):
+#     if solve(i) == easy(i):
+#         continue
+#     print(i, solve(i), easy(i))
