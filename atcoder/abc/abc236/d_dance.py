cb8359b90000f7311b06315b1370b08da57e4858
yayo256 <yojiyama7@gmail.com>
2022-05-03 12:18:37 +0900

    s

--- atcoder/abc/abc236/d_dance.py
+++ atcoder/abc/abc236/d_dance.py
@@ -1,3 +1,40 @@
+N = int(input())
+A = [[None]*(i+1) + list(map(int, input().split())) for i in range(2*N-1)]
+
+
+
+################################
+
+# N = int(input())
+# A = [[None]*(i+1) + list(map(int, input().split())) for i in range(2*N-1)]
+
+# # n%2 == 0
+# def p1(n, zcnt=0):
+#     if n == 0:
+#         return ['']
+#     ans_list = []
+#     if n-zcnt > 0:
+#         ans_list += ['('+s for s in p1(n-1, zcnt+1)]
+#     if zcnt > 0:
+#         ans_list += [')'+s for s in p1(n-1, zcnt-1)]
+#     return ans_list
+
+# print(len(p1(2*N)))
+
+# max_score = 0
+# for s in p1(2*N):
+#     stack = []
+#     score = 0
+#     for j, c in enumerate(s):
+#         if c == '(':
+#             stack.append(j)
+#         else:
+#             a, b = stack.pop(), j
+#             score ^= A[a][b]
+#     max_score = max(score, max_score)
+
+# print(max_score)
+
 ################################
 
 # N = int(input())
