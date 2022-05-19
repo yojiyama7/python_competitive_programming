9131fecdabf575c564e248015fb42215d6545bc9
yojiyama7 <yojiyama7@gmail.com>
2022-05-19 14:10:14 +0900

    tgit

--- atcoder/abc/abc236/d_dance.py
+++ atcoder/abc/abc236/d_dance.py
@@ -1,7 +1,37 @@
 N = int(input())
 A = [[None]*(i+1) + list(map(int, input().split())) for i in range(2*N-1)]
 
+to_digit_idx = dict(((1<<i), i) for i in range(20))
+memo = [None for i in range(1<<2*N)]
+def solve(n, state):
+    # print((n, state))
+    if state == 0:
+        return [[]]
+    if memo[state] != None:
+        return memo[state]
+    ans = []
+    d = state&-state
+    state &= ~d
+    a = to_digit_idx[d]
+    # print([a])
+    for i in range(n):
+        if (state>>i)&1:
+            p = (a, i)
+            for li in solve(n, state & ~(1<<i)):
+                ans.append([p] + li)
+    memo[state] = ans
+    return memo[state]
 
+ans = 0
+for p in solve(2*N, (1<<2*N)-1):
+    nums = [A[a][b] for a, b in p]
+    # print(nums)
+    score = 0
+    for n in nums:
+        score ^= n
+    ans = max(ans, score)
+
+print(ans)
 
 ################################
 
